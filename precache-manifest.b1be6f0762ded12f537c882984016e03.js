40eec2a6541d370960094e7f0ae6f96973cec2af
vishrudhrajrs14 <vishrudhrajrs14.com>
2020-10-15 12:16:47 +0530

    Updates

---
+++ precache-manifest.b1be6f0762ded12f537c882984016e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b4c8b61da225e09789db784051f7a",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "00de452c1e6a84a73d02",
+    "url": "/whatsapp-clone/static/css/main.a48c9141.chunk.css"
+  },
+  {
+    "revision": "f8eab238264841a4e2bc",
+    "url": "/whatsapp-clone/static/js/2.c42a9e6d.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.c42a9e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00de452c1e6a84a73d02",
+    "url": "/whatsapp-clone/static/js/main.46c254f4.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);
